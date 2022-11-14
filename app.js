115768a9fa7030e446de7bfc5a7064c38259c2b7
brian22550 <112784468+brian22550@users.noreply.github.com>
2022-11-13 23:39:32 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+let boton = document.getElementById('sumar');
+let respuesta = document.getElementById('respuesta');
+
+boton.addEventListener('click',hacerSuma);
+
+function hacerSuma(){
+let n2=parseFloat(document.getElementById('n2').value);
+const entrada=200
+let s=n2*entrada;
+respuesta.innerHTML=s
+}
 
